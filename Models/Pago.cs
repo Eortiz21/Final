ca4cda8ece014541eb4824c25268ae17f09dd493
Salvador <santossalvador413@gmail.com>
2025-09-07 17:25:46 -0600

    Crear pago
    Diseño de pago

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -2,30 +2,30 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Primera.Models
-{
-    public class Pago
+    namespace Primera.Models
     {
-        [Key]
-        public int Id_Pago { get; set; }
+        public class Pago
+        {
+            [Key]
+            public int Id_Pago { get; set; }
 
-        [ForeignKey("Ticket")]
-        public int Id_Ticket { get; set; }
-        public Ticket Ticket { get; set; }
+            [ForeignKey("Ticket")]
+            public int Id_Ticket { get; set; }
+            public Ticket Ticket { get; set; }
 
-        [Required]
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal MontoPago { get; set; }
+            [Required]
+            [Column(TypeName = "decimal(18,2)")]
+            public decimal MontoPago { get; set; }
 
-        [Required]
-        public DateTime FechaPago { get; set; }
+            [Required]
+            public DateTime FechaPago { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string MetodoPago { get; set; }
+            [Required]
+            [StringLength(50)]
+            public string MetodoPago { get; set; }
 
-        [Required]
-        [StringLength(30)]
-        public string EstadoPago { get; set; }
+            [Required]
+            [StringLength(30)]
+            public string EstadoPago { get; set; }
+        }
     }
-}
