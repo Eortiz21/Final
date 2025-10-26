ade506d077e431bdafe4972006ecb6ff509c96e3
bhsalguero <bryansalguero703@gmail.com>
2025-10-26 15:10:26 -0600

    SUTIL

--- Migrations/20251026134942_Inicial.cs
+++ Migrations/20251026134942_Inicial.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Primera.Migrations
 {
     /// <inheritdoc />
-    public partial class mejora : Migration
+    public partial class Inicial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
